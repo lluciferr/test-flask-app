a2fa79d5ce5417e8831f6bbdc755eeb356c57451
lluciferr <82355776+lluciferr@users.noreply.github.com>
2023-04-03 21:06:11 +0545

    done once more

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Flask app working properly and well done ’
+   return 'Flask app working properly and well done'
 
 if __name__ == '__main__':
    app.run()
